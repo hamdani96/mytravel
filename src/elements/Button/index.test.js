d29933fd79b8c9d2c20314f09a4de4f2847f79b9
Hamdani <58839721+hamdani96@users.noreply.github.com>
2022-06-22 16:59:23 +0700

    fix test for component button

--- src/elements/Button/index.test.js
+++ src/elements/Button/index.test.js
@@ -1,4 +1,9 @@
 import React from "react";
-import { render } from "@testing-library/jest-dom/matchers";
+import { render } from "@testing-library/react";
 import Button from "./index";
 
+test("Should not allowed click button isDisabled is present", () => {
+    const { container } = render(<Button isDisabled={true}></Button>);
+
+    expect(container.querySelector("span.disabled")).toBeInTheDocument();
+})
